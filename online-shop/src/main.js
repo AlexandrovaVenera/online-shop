1cd8ffe19963f732e8b85abcf693cb0ce5403581
Faizieva Venera <v.faizyeva@gmail.com>
2024-03-11 23:27:07 +0300

    feat: add Vue Router

--- online-shop/src/main.js
+++ online-shop/src/main.js
@@ -3,5 +3,21 @@ import './assets/main.css'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './pages/Home.vue'
+import Drawer from './components/Drawer.vue'
 
-createApp(App).use(createPinia()).mount('#app')
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/drawer', name: 'Drawer', component: Drawer }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(createPinia()).mount('#app')
+
+app.use(router)
